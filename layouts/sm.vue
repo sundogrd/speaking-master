1a40cac6c123d2f31fc9e9e4770a2f3e58c73949
lwyj123 <443474713@qq.com>
2018-09-01 20:14:26 +0800

    feat: add sm layout

---
+++ layouts/sm.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-app
+    id="inspire"
+    dark
+  >
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      app
+    >
+      <v-list dense>
+        <v-list-tile v-for="item in items" :key="item.text">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-subheader class="mt-3 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+        <v-list>
+          <v-list-tile v-for="item in items2" :key="item.text" avatar>
+            <v-list-tile-avatar>
+              <img :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`" alt="">
+            </v-list-tile-avatar>
+            <v-list-tile-title v-text="item.text"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+        <v-list-tile class="mt-3">
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">add_circle_outline</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Browse Channels</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon color="grey darken-1">settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="grey--text text--darken-1">Manage Subscriptions</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      color="red"
+      dense
+      fixed
+      clipped-left
+      app
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-icon class="mx-3">fab fa-youtube</v-icon>
+      <v-toolbar-title class="mr-5 align-center">
+        <span class="title">Youtube</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-layout row align-center style="max-width: 650px">
+        <v-text-field
+          @click:append="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="search"
+          color="white"
+          hide-details
+        ></v-text-field>
+      </v-layout>
+    </v-toolbar>
+    <v-content>
+      <v-container fill-height>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-bottom-nav
+        :active.sync="bottomNav"
+        :color="color"
+        :value="true"
+        fixed
+        shift
+      >
+        <v-btn dark>
+          <span>Video</span>
+          <v-icon>ondemand_video</v-icon>
+        </v-btn>
+
+        <v-btn dark>
+          <span>Music</span>
+          <v-icon>music_note</v-icon>
+        </v-btn>
+
+        <v-btn dark>
+          <span>Book</span>
+          <v-icon>book</v-icon>
+        </v-btn>
+
+        <v-btn dark>
+          <span>Image</span>
+          <v-icon>image</v-icon>
+        </v-btn>
+    </v-bottom-nav>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: true,
+    bottomNav: 3,
+    items: [
+      { icon: "trending_up", text: "Most Popular" },
+      { icon: "subscriptions", text: "Subscriptions" },
+      { icon: "history", text: "History" },
+      { icon: "featured_play_list", text: "Playlists" },
+      { icon: "watch_later", text: "Watch Later" }
+    ],
+    items2: [
+      { picture: 28, text: "Joseph" },
+      { picture: 38, text: "Apple" },
+      { picture: 48, text: "Xbox Ahoy" },
+      { picture: 58, text: "Nokia" },
+      { picture: 78, text: "MKBHD" }
+    ]
+  }),
+  props: {
+    source: String
+  },
+  computed: {
+    color () {
+      switch (this.bottomNav) {
+        case 0: return 'blue-grey'
+        case 1: return 'teal'
+        case 2: return 'brown'
+        case 3: return 'indigo'
+      }
+    }
+  }
+};
+</script>
